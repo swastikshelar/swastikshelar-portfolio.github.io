1a418c11d3e4d8c185c8bc73a5587f8c12bf7767
swastikshelar <102980626+swastikshelar@users.noreply.github.com>
2023-03-21 13:57:53 +0530

    Swastik-Portfolio

---
+++ script.js
@@ -0,0 +1,40 @@
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+//--  STICKY NAVBAR --//
+
+window.onscroll = function() {myFunction()};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//--  HAMBURGER--//
+
+    const burgerMenu = document.querySelector('.burgerMenu');
+    const navbarr = document.querySelector('.navbarr');
+
+    burgerMenu.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
